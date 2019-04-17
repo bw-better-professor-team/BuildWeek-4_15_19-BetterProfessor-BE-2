9290e1e8c8eb42f852ee0d12719d177740e1438e
lilvina <davinaleo2007@yahoo.com>
2019-04-17 15:49:00 -0700

    worked on routes for professor

--- api/server.js
+++ api/server.js
@@ -10,6 +10,7 @@ const registerRouter = require('../routes/register-router.js')
 const userRouter = require('../routes/user-router.js')
 const studentsRouter = require('../routes/students-router.js')
 const projectsRouter = require('../routes/projects-router.js')
+const professorStudentRouter = require('../routes/professor-student-router.js')
 
 server.use(helmet())
 server.use(express.json())
@@ -20,5 +21,6 @@ server.use('/api/register', registerRouter)
 server.use('/api/users', userRouter)
 server.use('/api/students', studentsRouter)
 server.use('/api/projects', projectsRouter)
+server.use('/api/professors-students', professorStudentRouter)
 
 module.exports = server
